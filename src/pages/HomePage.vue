f3b70c8ed8ed214b5d7454e0fc2d116db2829c37
black <blsmithit@gmail.com>
2024-05-08 12:15:23 +0300

    base home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,5 @@
 <template>
   <v-container>
-    <v-container>
       <v-row>
         <v-col cols="6">
           <v-card class="pt-1" v-for="item in items" :key="item.name" color="black">
@@ -15,61 +14,72 @@
           </v-btn>
         </v-col>
       </v-row>
-
-      <v-row>
-        <v-col cols="3">
-      <v-card class="pt-5"
-              elevation="0"
-              @click="redirectToInviteFriendPage()"
-              style="z-index: 1"
-              variant="tonal"
-              rounded="xl"
-              height="100"
-              width="100"
-              color="white">
+    <v-container>
+  <v-row>
+    <v-col cols="12">
+      <div class="overflow-x-auto" style="width: 100%">
+        <v-row>
+          <v-col cols="3">
+            <v-card class="pt-5"
+                    elevation="0"
+                    @click="redirectToInviteFriendPage()"
+                    style="z-index: 1"
+                    variant="tonal"
+                    rounded="xl"
+                    height="100"
+                    width="100"
+                    color="white">
               <v-card-text class="bg-transparent text-center py-0 px-0">
                 <label class="theme-label-hint-color" style="font-size: 12px">
                   Траты в ресторанах
                 </label>
               </v-card-text>
-      </v-card>
-        </v-col>
+            </v-card>
+          </v-col>
 
-        <v-col cols="3">
-          <v-card class="pt-5 ml-8"
-              elevation="0"
-              @click="redirectToInviteFriendPage()"
-              style="z-index: 1"
-              variant="tonal"
-              rounded="xl"
-              height="100"
-              width="100"
-              color="white">
+          <v-col cols="3">
+            <v-card class="pt-5 ml-6"
+                    elevation="0"
+                    @click="redirectToInviteFriendPage()"
+                    style="z-index: 1"
+                    variant="tonal"
+                    rounded="xl"
+                    height="100"
+                    width="100"
+                    color="white">
               <v-card-text class="bg-transparent text-center py-0 px-0">
                 <label class="theme-label-hint-color" style="font-size: 12px">
                   На что потратить
                 </label>
               </v-card-text>
-          </v-card>
-        </v-col>
-                <v-col cols="3">
-          <v-card class="pt-5 ml-16"
-              elevation="0"
-              @click="redirectToInviteFriendPage()"
-              style="z-index: 1"
-              variant="tonal"
-              rounded="xl"
-              height="100"
-              width="100"
-              color="white">
+            </v-card>
+          </v-col>
+
+          <v-col cols="3">
+            <v-card class="pt-5 ml-12"
+                    elevation="0"
+                    @click="redirectToInviteFriendPage()"
+                    style="z-index: 1"
+                    variant="tonal"
+                    rounded="xl"
+                    height="100"
+                    width="100"
+                    color="white">
               <v-card-text class="bg-transparent text-center py-0 px-0">
                 <label class="theme-label-hint-color" style="font-size: 12px">
                   Как <br> сэкономить деньги
                 </label>
               </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
+            </v-card>
+          </v-col>
+
+          <!-- Add more cards here -->
+
+        </v-row>
+      </div>
+    </v-col>
+  </v-row>
+</v-container>
 
       <v-row>
         <v-col cols="6">
@@ -82,16 +92,16 @@
               height="130"
               width="160"
               color="white">
-              <v-card-text class="bg-transparent text-center py-0 px-0">
+              <v-card-text class="bg-transparent text-center py-0 px-0" v-for="item in items" :key="item.name">
                 <label class="theme-label-hint-color" style="font-size: 16px">
                   Все операции <br>
                 </label>
                 <label class="theme-label-hint-color" style="font-size: 10px">
-                  Трат в мае: 100 ₽
+                  Трат в мае: {{item.spending}} ₽
                 </label>
               </v-card-text>
           </v-card>
-          </v-col>
+        </v-col>
 
         <v-col cols="6">
           <v-card class="pt-5"
@@ -105,15 +115,34 @@
               color="white">
               <v-card-text class="bg-transparent text-center py-0 px-0">
                 <label class="theme-label-hint-color" style="font-size: 16px">
-                  Cashback и бонусы
+                  Посмотреть <br> Ton
                 </label>
               </v-card-text>
             </v-card>
           </v-col>
       </v-row>
-
-
-    </v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-card class="pt-1" v-for="item in items" :key="item.name" color="white" height="100" width="350" rounded="xl">
+          <v-card-text style="font-size: 18px">
+            {{item.balance}} ₽
+          </v-card-text>
+          <label class="ml-4"> Bank account </label>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row class="d-flex justify-center">
+      <v-col cols="6">
+        <v-btn variant="tonal" style="font-size: 11px" rounded="xl">
+          Пополнить баланс
+        </v-btn>
+      </v-col>
+      <v-col cols="6">
+        <v-btn variant="tonal" style="font-size: 11px" rounded="xl">
+          Перевести деньги
+        </v-btn>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -125,11 +154,10 @@ export default {
   components: {
   },
   data: () => ({
-    tg_platform: null,
-    user_id: null,
-    page_loading: true,
     items: [{
       name: 'Vadim',
+      balance: 355,
+      spending: 123
     }]
   }),
   mounted() {
